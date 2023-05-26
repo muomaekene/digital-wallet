026acd52b6fe0a2cb80b643a6e7bf180eb8c805a
Ekene Muoma <muomaekene@gmail.com>
2023-05-26 13:13:24 -0400

    Initialize project, implement authentication and create models

--- backend_api/app.js
+++ backend_api/app.js
@@ -1,17 +1,32 @@
-require("dotenv").config();
-require("./config/database").connect();
+const path = require("path");
 const express = require("express");
+const cors = require("cors");
 
 const loginRoute = require("./routes/login");
 const registerRoute = require("./routes/register");
 
+require("dotenv").config();
+require("./config/database").connect();
+
 const app = express();
 
+const corsOptions = {
+  origin: "https://checkout-v3-ui-prod.f4b-flutterwave.com",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Digital wallet API");
+});
+
+app.get("/pay", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
 app.post("/register", registerRoute);
 app.post("/login", loginRoute);
-
-// Logic here
 
 module.exports = app;
